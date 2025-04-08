609d348de95656ff549ebf33fc713650f7c83126
Gabriel Alves Toyo <ggabrieltoyo@gmail.com>
2025-04-08 13:20:10 -0300

    refactor: organizando a arquitetura das pastas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectorRef, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
